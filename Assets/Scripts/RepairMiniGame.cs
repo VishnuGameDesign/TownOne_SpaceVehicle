01fe278d66d4171e151bdc2337fb3a66626e7034
VishnuGD75 <vishnus592000@gmail.com>
2024-11-09 12:45:54 -0800

    minigame01-test ready

--- Assets/Scripts/RepairMiniGame.cs
+++ Assets/Scripts/RepairMiniGame.cs
@@ -1,5 +1,6 @@
-using System.Collections;
+using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RepairMiniGame : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class RepairMiniGame : MonoBehaviour
 
     [SerializeField] private float _smallerBarSpeed = 1.5f;
     [SerializeField] private float _biggerBarSpeed = 2f;
+    [SerializeField] private float _toMatchThreshold = .1f;
+    [SerializeField] private PlayerInput _playerInput;
 
     private void Start()
     {
@@ -20,6 +23,10 @@ public class RepairMiniGame : MonoBehaviour
     private void Update()
     {
         MoveTarget();
+        if (Input.GetKeyDown(KeyCode.R) && TargetsMatched())
+        {
+            Debug.Log("REPAIR DONE");
+        }
     }
 
     private void MoveTarget()
@@ -31,6 +38,10 @@ public class RepairMiniGame : MonoBehaviour
         _movingTarget.transform.position = Vector3.Lerp(_leftEndPivotPoint.position, _rightEndPivotPoint.position, pingPongForSmallBar / distance);
         _matchingTarget.transform.position = Vector3.Lerp(_rightEndPivotPoint.position, _leftEndPivotPoint.position, pingPongForBigBar / distance);
     }
+
     
-    
+    private bool TargetsMatched()
+    {
+        return Vector2.Distance(_leftEndPivotPoint.position, _matchingTarget.transform.position) < _toMatchThreshold;
+    }
 }
