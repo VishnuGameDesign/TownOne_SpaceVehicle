029c6d7d07c4d49c01877769817b4ea57db4921f
Andy <sjandybae@gmail.com>
2024-11-10 12:00:13 -0800

    functional NPC spawning

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -6,18 +6,71 @@ using UnityEngine;
 
 public class NPCManager : MonoBehaviour
 {
-    [SerializeField] String CharacterID;
+    public GameObject[] npcs;
+    public int current_npc_index = 0;
+    public int number_visible = 0;
+    private int number_visible_max = 3;
+    public Vector2[] npc_positions;
+    public int current_npc_position_index = 0;
+    public Vector2[] spawn_points;
+    private float spawn_timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        for(int i = 0; i < npcs.Length; i++)
+        {
+            print("Found: " + npcs[i].name);
+            if (i < number_visible_max)
+            {
+                npc_positions[i] = npcs[i].transform.position;
+            }
+            if (npcs[i].GetComponent<NPCMovement>())
+            {
+                npcs[i].GetComponent<NPCMovement>().my_manager = this;
+            }
+            npcs[i].SetActive(false);
+        }
+        current_npc_index = 1;
+        current_npc_position_index = 0;
+        number_visible = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (spawn_timer <= 0)
+        {
+            GameObject current_npc = npcs[current_npc_index];
+            if (current_npc.GetComponent<NPCMovement>())
+            {
+                if (current_npc.GetComponent<NPCMovement>().current_state == "inactive")
+                {
+                    current_npc.transform.position = spawn_points[UnityEngine.Random.Range(0, spawn_points.Length)];
+                    print(current_npc_index);
+                    print(current_npc_position_index);
+                    current_npc.GetComponent<NPCMovement>().start_position = current_npc.transform.position;
+                    current_npc.GetComponent<NPCMovement>().target_position = npc_positions[current_npc_position_index];
+                    current_npc.GetComponent<NPCMovement>().current_state = "entering";
+                    current_npc.SetActive(true);
+
+                    current_npc_index++;
+                    if (current_npc_index > npcs.Length - 1) current_npc_index = 0;
+                    current_npc_position_index++;
+                    if (current_npc_position_index > npc_positions.Length - 1) current_npc_position_index = 0;
+
+                    number_visible++;
+                    spawn_timer = 4f * number_visible;
+                }
+            }
+       }
+       else
+       {
+            if (number_visible < number_visible_max)
+            {
+                spawn_timer -= Time.deltaTime;
+            }
+       }
     }
 
     
---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    public NPCManager my_manager;
+    public Vector2 start_position;
+    public Vector2 target_position;
+    public string current_state;
+    
+    public float walk_speed = 5f;
+    private float idle_timer = 0;
+    private Vector2 target_position_offset = new Vector2(0f, 0f);
+    private float timer_before_leaving = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_state = "inactive";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(current_state)
+        {
+            case "entering":
+                //face direction
+                if (GetComponent<SpriteRenderer>())
+                {
+                    if (transform.position.x > target_position.x) GetComponent<SpriteRenderer>().flipX = true;
+                    else GetComponent<SpriteRenderer>().flipX = false;
+                }
+
+                // move towards the target location
+                transform.position = Vector2.MoveTowards(transform.position, target_position, walk_speed * Time.deltaTime);
+                if(Vector2.Distance(transform.position, target_position) < 1f)
+                {
+                    current_state = "idle";
+                    idle_timer = 5f;
+                    timer_before_leaving = 10f;
+                }
+                break;
+
+            case "idle":
+                timer_before_leaving -= Time.deltaTime;
+                if (timer_before_leaving <= 0f)
+                {
+                    current_state = "leaving";
+                }
+                else
+                {
+                    if (idle_timer <= 0f)
+                    {
+                        if (target_position_offset == new Vector2(0f, 0f))
+                        {
+                            target_position_offset = new Vector2(transform.position.x + UnityEngine.Random.Range(-3.0f, 3.0f), transform.position.y + UnityEngine.Random.Range(-3.0f, 3.0f));
+                        }
+                        //face direction
+                        if (GetComponent<SpriteRenderer>())
+                        {
+                            if (transform.position.x > target_position_offset.x) GetComponent<SpriteRenderer>().flipX = true;
+                            else GetComponent<SpriteRenderer>().flipX = false;
+                        }
+                        
+                        // move towards the target location
+                        transform.position = Vector2.MoveTowards(transform.position, target_position_offset, walk_speed * Time.deltaTime);
+                        if(Vector2.Distance(transform.position, target_position_offset) < 1f)
+                        {
+                           idle_timer = 5f;
+                        }
+                    }
+                    else
+                    {
+                        idle_timer -= Time.deltaTime;
+                        if (target_position_offset != new Vector2(0f, 0f))
+                        {
+                            target_position_offset = new Vector2(0f, 0f);
+                        }
+                    }
+                }
+                break;
+
+            case "talking":
+                break;
+
+            case "leaving":
+                //face direction
+                if (GetComponent<SpriteRenderer>())
+                {
+                    if (transform.position.x > start_position.x) GetComponent<SpriteRenderer>().flipX = true;
+                    else GetComponent<SpriteRenderer>().flipX = false;
+                }
+
+                // move towards the target location
+                transform.position = Vector2.MoveTowards(transform.position, start_position, walk_speed * Time.deltaTime);
+                if(Vector2.Distance(transform.position, start_position) < 1f)
+                {
+                    current_state = "inactive";
+                    gameObject.SetActive(false);
+                    my_manager.number_visible--;
+                }
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+}
