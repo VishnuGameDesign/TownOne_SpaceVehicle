079d8daee9ab3760400b6696b8bdfc79e67a065e
Andy <sjandybae@gmail.com>
2024-11-10 16:07:49 -0800

    music added to title

--- Assets/MenuScreen.cs
+++ Assets/MenuScreen.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class MenuScreen : MonoBehaviour
 {
     private Vector3 target_transform = new Vector3(0f, 0f, 0f);
+    [field: SerializeField] private AudioSource AudioSource { get; set; }
+
+    private void OnValidate()
+    {
+        if(AudioSource == null) AudioSource = GetComponent<AudioSource>();
+    }
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0f, -10f, 0f);
+        if (AudioSource != null) {
+            AudioSource.Play();
+        }
     }
 
     // Update is called once per frame
